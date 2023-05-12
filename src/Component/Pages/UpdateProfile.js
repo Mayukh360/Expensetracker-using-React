34b3f54e5674f48aa56c9e02dd5fe61dfd5e8928
Mayukh <mayukhchatterjee2023@gmail.com>
2023-05-12 21:31:09 +0530

    GET request

--- src/Component/Pages/UpdateProfile.js
+++ src/Component/Pages/UpdateProfile.js
@@ -1,15 +1,82 @@
-import React from 'react'
+import React,{useRef,useEffect} from 'react'
 
 export default function UpdateProfile() {
+    const formRef=useRef();
+    const token=localStorage.getItem('token');
+    const submitHandler=(event)=>{
+        event.preventDefault();
+    const fullName=formRef.current.elements.name.value;
+    const photoUrl=formRef.current.elements.profilephoto.value;
+    console.log(fullName,photoUrl);
+
+    
+
+    const data = {
+        idToken:token,
+        displayName: fullName,
+        photoUrl: photoUrl,
+        deleteAttribute:["DISPLAY_NAME", "PHOTO_URL"],
+        returnSecureToken:true,
+      };
+ console.log(data);
+      fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyDXx5szR2zhd9OQlqBegt7PJUE8RXQAqAk',
+      {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response
+        console.log('Profile updated:', data);
+      })
+      .catch((error) => {
+        console.error('Error updating profile:', error);
+      });
+    }
+
+    async function fetchData(){
+        console.log("TOKEN",token);
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDXx5szR2zhd9OQlqBegt7PJUE8RXQAqAk',{
+            method: 'POST',
+           
+            headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                idToken: token,
+              }),
+        })
+        .then((response)=>{
+            console.log(response)
+         return   response.json()
+            
+        })
+        .then((data)=>{
+            console.log('Fetched Data',data);
+        })
+        .catch((error) => {
+            console.error('Error updating profile:', error);
+          });
+    }
+    useEffect(()=>{
+        fetchData();
+    },[])
+
   return (
     <div>
         <h2>Winners never quit, quitters never win</h2>
         <label>Contact Details</label><br/><br/>
-        <form>
+        <form ref={formRef} onSubmit={submitHandler}>
         <label>Full Name</label>
-        <input type='text'/>
+        <input type='text' id='name'/>
         <label>Profile Photo URL</label>
-        <input type='text'/>
+        <input type='text' id='profilephoto'/>
+        <button>Update</button>
         </form>
     </div>
   )
