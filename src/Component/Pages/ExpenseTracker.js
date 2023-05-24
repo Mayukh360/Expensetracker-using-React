0d0899075af1b6607ee5541620f3f12a211f23db
Mayukh <mayukhchatterjee2023@gmail.com>
2023-05-24 19:11:50 +0530

    Minor Changes

--- src/Component/Pages/ExpenseTracker.js
+++ src/Component/Pages/ExpenseTracker.js
@@ -2,14 +2,14 @@ import React, { useRef, useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { authActions } from "../../storee/AuthReducer";
 import { useSelector } from "react-redux";
-import './ExpenseTracker.css'
+import "./ExpenseTracker.css";
 
 export default function ExpenseTracker() {
   const formRef = useRef();
   const dispatch = useDispatch();
   const [expenses, setExpenses] = useState([]);
   const isToggle = useSelector((state) => state.auth.darkToggle);
-  const enteredEmail=localStorage.getItem('email');
+  const enteredEmail = localStorage.getItem("email");
   const changedemail = enteredEmail.replace("@", "").replace(".", "");
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -29,9 +29,6 @@ export default function ExpenseTracker() {
         }
       })
       .then((data) => {
-        // console.log("USEEFFECT", data);
-        // console.log(data);
-        //Update the data using setAmount and setDescription
         const fetchedExpenses = [];
         for (const key in data) {
           fetchedExpenses.push({
@@ -50,7 +47,7 @@ export default function ExpenseTracker() {
 
   async function submitHandler(event) {
     event.preventDefault();
-   
+
     const amountInput = formRef.current.elements.amount.value;
     const descriptionInput = formRef.current.elements.description.value;
     const categoryInput = formRef.current.elements.category.value;
@@ -62,8 +59,6 @@ export default function ExpenseTracker() {
       description: descriptionInput,
       category: categoryInput,
     };
-    // setExpenses([expenseData]);
-    // setExpenses((prevExpenses) => [...prevExpenses, expenseData]);
 
     await fetch(
       `https://authanticate-form-default-rtdb.firebaseio.com/user/${changedemail}.json`,
@@ -88,8 +83,7 @@ export default function ExpenseTracker() {
       })
       .then((data) => {
         console.log("USEEFFECT", data);
-        // console.log(data);
-        //Update the data using setAmount and setDescription
+
         const fetchedExpenses = [];
         for (const key in data) {
           fetchedExpenses.push({
@@ -107,7 +101,7 @@ export default function ExpenseTracker() {
   }
 
   const dltbtnHandler = (expenseId) => {
-    console.log("ID",expenseId);
+    console.log("ID", expenseId);
     setExpenses((prevExpenses) =>
       prevExpenses.filter((expense) => expense.id !== expenseId)
     );
@@ -120,12 +114,6 @@ export default function ExpenseTracker() {
       .then((response) => {
         if (response.ok) {
           console.log("Expense deleted successfully!");
-          //Without state update useeffect won't run
-          //The callback function (expense) => expense.id !== expenseId checks if the id of the expense object is not equal to the expenseId we want to delete.
-          // If the condition is true, the expense object is kept in the new array. If the condition is false, the expense object is excluded from the new array.
-          // setExpenses((prevExpenses) =>
-          //   prevExpenses.filter((expense) => expense.id !== expenseId)
-          // );
         } else {
           console.log("Failed to delete expense");
         }
@@ -154,9 +142,6 @@ export default function ExpenseTracker() {
         .then((response) => {
           if (response.ok) {
             console.log("Expense deleted successfully!");
-            // setExpenses((prevExpenses) =>
-            //   prevExpenses.filter((expense) => expense.id !== expenseId)
-            // );
           } else {
             console.log("Failed to delete expense");
           }
@@ -175,8 +160,7 @@ export default function ExpenseTracker() {
   if (sum) {
     dispatch(authActions.ispremium(sum));
   }
-  // setTotalExpense(sum);
-  // console.log(sum)
+
   function downloadExpensesAsTxt() {
     const data = expenses.map((expense) => {
       return `Amount: ${expense.amount} | Description: ${expense.description} | Category: ${expense.category}`;
@@ -200,13 +184,6 @@ export default function ExpenseTracker() {
           Dark Theme Activated
         </h1>
       )}
-
-      {/* <button
-        onClick={downloadExpensesAsTxt}
-        className="bg-blue-500 text-white font-medium py-2 px-4 rounded"
-      >
-        Download File
-      </button> */}
 
       {!isToggle && (
         <div>
@@ -272,7 +249,7 @@ export default function ExpenseTracker() {
                   onClick={() => {
                     dltbtnHandler(expense.id);
                   }}
-                  className="ml-2 bg-red-500 hover:bg-red-600 text-white font-medium py-2 px-4 rounded"
+                  className="ml-2   bg-red-500 hover:bg-red-600 text-white font-medium py-2 px-4 rounded  "
                 >
                   Delete
                 </button>
@@ -280,7 +257,7 @@ export default function ExpenseTracker() {
                   onClick={() => {
                     editbtnhandler(expense.id);
                   }}
-                  className="ml-2 bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded"
+                  className="ml-2  bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded "
                 >
                   Edit
                 </button>
